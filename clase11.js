0bf76f9f9783056668a225a13d56989fb8f50b75
DanteHechenleitner <hechenleitnerdante@gmail.com>
2022-09-29 13:02:11 -0300

    Desafio Clase 13

--- clase11.js
+++ clase11.js
@@ -1,26 +1,16 @@
 
-
-
+let formulario = document.getElementById("formulario");
+let verStock = document.getElementById("verStock");
+let guardarProducto = document.getElementById("guardar");
 
 /// creo un bucle para poder contar el array y generar los ID
 
 function crearID(){
     let contador = JSON.parse(localStorage.getItem("Producto"))
+    let creadId = contador == null ? contador = 0 : contador.length 
 
-    ///corresponde a la enterga de la clase 12
-    let prueba = contador == null ? contador = 0 : contador.length 
-
-    return prueba
-    //console.log(contador.length)
-    /*if( contador == null){
-        return 0
-    }else{
-        return contador.length
-    }*/
+    return creadId
 }
-
-let formulario = document.getElementById("formulario")
-
 
 //// Se crea la Clase Producto para poder crear con New nuevos productos
 
@@ -35,18 +25,22 @@ class Productos{
     }
 }
 
+// JS Librerias
+
+function alertGuardado(){
+    Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'Cotizacion guardada',
+        showConfirmButton: false,
+        timer: 1500
+    })
+}
 
 
-/// Profe aca quise realizar una funcion para validar si los datos que se cargar eran coretos
-/// pero no pude, se me guaradaba en el storage de todas formas
-/// lo quise hacer con deletitem 
-/// se que primero tendria que tomar el storage transformarlo a JS, recorrerlo y buscar el el array que no esta bien
-/// borrarlo y actualizar el arrar no? Capaz me estoy complicando demasiado 
-//// Lo pude resolver, espero que este bien
+//// Validar los valores que ingresa el usuario por HTML
 
-
-///corresponde a la enterga de la clase 12
-function validar(valor){
+function validarHtml(valor){
 
 
     if( valor.value == null || valor.value == false || valor.value == ""){
@@ -56,15 +50,9 @@ function validar(valor){
         nodoSmall.innerText = "Datos mal ingresado"
         
     }else if ( valor.value != null || valor.value != false || valor.value != ""){
-        let control = valor.parentElement
-        
+        let control = valor.parentElement      
         control.className = "mensajeError"
         
-    }else{
-        let nodoSmall = control.querySelector('small');
-        nodoSmall.innerHTML = ""
-        control.className = "mensajeError"
-        formulario.reset()
     }
 
 }
@@ -81,53 +69,40 @@ function agregarProductos(){
     const cantidad = document.getElementById("cantidad");
     const precio = document.getElementById("precio");
     
-    //const nuevoProducto = new Productos(id, codigo.value, producto.value, cantidad.value, precio.value);
-
-    /*if(codigo.value == "" || producto.value == "" || cantidad.value || precio.value){
-        validar(codigo)
-        validar(producto)
-        validar(cantidad)
-        validar(precio)
-    }*/
-
    
     const productosEnStorage = JSON.parse(localStorage.getItem("Producto"))
-    
-    /// Aca trate de hacer una validacion, solo valide el campo CODIGO, por no llegaba
-    /// no se si esta biem, pero para darle un poco más de onda cree un small en el HTML
-    /// para que me diga que el hay que cargar si o si el codigo
-
 
 
     if(codigo.value == "" || producto.value == "" || cantidad.value == "" || precio.value == ""){
-        validar(codigo)
-        validar(producto)
-        validar(cantidad)
-        validar(precio)
-       //let controlC = document.getElementById("codigo").parentElement
-       //let nodoSmall = controlC.querySelector('small');
-       //controlC.className = "mensajeError Visible"
-       //nodoSmall.innerText = "Datos mal ingresado"
+        validarHtml(codigo)
+        validarHtml(producto)
+        validarHtml(cantidad)
+        validarHtml(precio)
 
     }else if(productosEnStorage == null){
+        validarHtml(codigo);
+        validarHtml(producto);
+        validarHtml(cantidad);
+        validarHtml(precio);
         const nuevoProducto = new Productos(id, codigo.value, producto.value, cantidad.value, precio.value);
         let arayProductos = []
         arayProductos.push(nuevoProducto)
         localStorage.setItem("Producto", JSON.stringify(arayProductos))
-        //let controlC = document.getElementById("codigo").parentElement
-        //controlC.className = "mensajeError"
-        //formulario.reset()
+        formulario.reset()
+        alertGuardado()
         
     }else{
+        validarHtml(codigo)
+        validarHtml(producto)
+        validarHtml(cantidad)
+        validarHtml(precio)
         const nuevoProducto = new Productos(id, codigo.value, producto.value, cantidad.value, precio.value);
         productosEnStorage.push(nuevoProducto)
         let enJASON = JSON.stringify(productosEnStorage)
         localStorage.setItem("Producto", enJASON)
-        //console.log(productosEnStorage)
-        //let controlC = document.getElementById("codigo").parentElement
-        //controlC.className = "mensajeError"
         formulario.reset()
-        
+        alertGuardado()
+         
     }
     
 
@@ -136,40 +111,48 @@ function agregarProductos(){
 
 
 /// tengo que recorrer lo que esta guardado en el storage para despues poder filtrar los productos
-///corresponde a la enterga de la clase 12
 
-function recorer(){
+function recorerStock(){
     let db = JSON.parse(localStorage.getItem("Producto"))
     let tdbody = document.getElementById("tdbody")
     console.log(db)
 
-    db.forEach( db => {
-        let td = document.createElement("tr")
-        td.innerHTML = `
-        <td data-title="#"> ${db.id}</td>
-        <td data-title="Codigo"> ${db.codigo}</td>
-        <td data-title="Producto"> ${db.producto}</td>
-        <td data-title="Cantidad"> ${db.cantidad}</td>
-        <td data-title="Precio"> $ ${db.precio}</td>
-        `
-        tdbody.appendChild(td)
-    })
-    
+    if(db != false && db != null){
+        db.forEach( db => {
+            let td = document.createElement("tr")
+            td.innerHTML = `
+            <td data-title="#"> ${db.id}</td>
+            <td data-title="Codigo"> ${db.codigo}</td>
+            <td data-title="Producto"> ${db.producto}</td>
+            <td data-title="Cantidad"> ${db.cantidad}</td>
+            <td data-title="Precio"> $ ${db.precio}</td>
+            `
+            tdbody.appendChild(td)
+        })
+    }else if (db == null){
+        return alertStorageVacio()
+        
+    }
+
    
 }
 
+// JS Librerias
 
-
-let ver = document.getElementById("ver")
-
-let guardarProducto = document.getElementById("guardar")
+function alertStorageVacio(){
+    Swal.fire({
+        position: 'top-end',
+        icon: 'warning',
+        title: 'No hay productos para mostrar',
+        showConfirmButton: false,
+        timer: 1500
+    })
+}
 
 
 guardarProducto != null && guardarProducto.addEventListener("click", agregarProductos)
 
-///corresponde a la enterga de la clase 12
-
-ver != null && ver.addEventListener("click", recorer)
+verStock != null && verStock.addEventListener("click", recorerStock)
 
 
 
